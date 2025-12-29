07514dfbfe1e71bf1093481a4a1e1194d2170aa7
Johan <johansalgut@gmail.com>
2025-12-29 20:10:56 +0100

    Inicialización de proyecto BankHelp

---
+++ bankhelp/src/main/java/com/slatdev/bankhelp/BankhelpApplication.java
@@ -0,0 +1,13 @@
+package com.slatdev.bankhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankhelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankhelpApplication.class, args);
+	}
+
+}
---
+++ bankhelp/src/test/java/com/slatdev/bankhelp/BankhelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slatdev.bankhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankhelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
